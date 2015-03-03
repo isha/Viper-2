808410ef85a35ebda1b150a76a0bb79fcaf2b036
Isha <isha.kumaarr@gmail.com>
2015-03-02 17:01:12 -0800

    Initial Commit

---
+++ Viper 2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Viper 2
+//
+//  Created by Isha on 2015-03-02.
+//  Copyright (c) 2015 Isha. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Viper 2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Viper 2
+//
+//  Created by Isha on 2015-03-02.
+//  Copyright (c) 2015 Isha. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
